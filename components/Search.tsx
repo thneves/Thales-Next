2ccd990e206886f0ca3f2a9290936bbaed92bf4d
thneves <tsneves11@gmail.com>
2021-11-30 19:53:49 -0300

    Add animation to tags

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { motion } from "framer-motion";
 import { tags } from "../constants/tags";
 import SearchImg from "../public/search.png";
+import { animate } from "tsparticles";
 
 
 const SearchBox = styled.div`
@@ -16,6 +17,7 @@ const SearchBox = styled.div`
 
 const Tags = styled(motion.button)`
   padding: 8px;
+  margin: 3px;
   border: 1px solid white;
   border-radius: 12px;
 `
@@ -25,18 +27,36 @@ const StyledDivTags = styled.div`
   margin: 0 auto;
 `
 
+const SearchText = styled(motion.p)`
+  font-size: 20px;
+  font-weight: lighter;
+  font-style: italic;
+  color: white;
+  text-shadow: 0px 0px 2px rgb(255, 255, 255);
+
+`
+
 const Search = () => {
 
   const [tag, setTag] = useState("");
 
   return (
     <SearchBox>
-    <Image src={SearchImg} alt="binocru" width={64} height={64} layout="fixed" />
-        <StyledDivTags>
+      <Image src={SearchImg} alt="binocru" width={64} height={64} layout="fixed" />
+      <SearchText
+        initial={{ x: -50 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 2}}
+      >Check something that interests you :)</SearchText>
+      <StyledDivTags>
         {tags.map(item => {
-        return <Tags>{item}</Tags>
-      })}
-    </StyledDivTags>
+        return <Tags
+          initial={{ x: 50 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8, repeat: 1 }}
+          >{item}</Tags>
+        })}
+      </StyledDivTags>
     </SearchBox>
   )
 }
