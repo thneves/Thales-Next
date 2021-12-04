d5c25beefd2901c6a37b06ab0e2e10c2369fcaf5
thneves <tsneves11@gmail.com>
2021-12-04 13:58:53 -0300

    Create skills card component

---
+++ components/SkillsCard.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledH3 = styled.h3`
+  color: blue;
+`
+
+const SkillsCard = () => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default SkillsCard;
---
+++ pages/skills.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Skills = () => {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export default Skills;
